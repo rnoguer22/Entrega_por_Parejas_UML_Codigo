a295bb402e1206760ff0ef49e68999cec79d88e0
rnoguer22 <rnogugon@myuax.com>
2022-03-17 18:22:36 +0100

    Copiamos enunciado del ejercicio

---
+++ Ejercicios/Puzzle.py
@@ -0,0 +1,17 @@
+class A: 
+    def z(self): 
+        return self 
+ 
+    def y(self, t): 
+        return len(t) 
+ 
+a = A 
+y = a.z 
+print(y(a)) 
+aa = a() 
+print(aa is a()) 
+z = aa.y 
+print(z(())) 
+print(a().y((a,))) 
+print(A.y(aa, (a,z))) 
+print(aa.y((z,1,'z')))
