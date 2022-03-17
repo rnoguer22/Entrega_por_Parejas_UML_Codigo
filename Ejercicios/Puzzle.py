fde183252dbda7fa2e6d57f2f75a394e52284b2f
rnoguer22 <rnogugon@myuax.com>
2022-03-17 18:46:06 +0100

    Explicacion del puzzle

--- Ejercicios/Puzzle.py
+++ Ejercicios/Puzzle.py
@@ -1,17 +1,17 @@
 class A: 
     def z(self): 
-        return self 
+        return self
  
     def y(self, t): 
         return len(t) 
  
 a = A 
-y = a.z 
-print(y(a)) 
+y = a.z
+print(y(a)) #Muestra por pantalla <class '__main__.A'> ya que la funcion z devuelve self
 aa = a() 
 print(aa is a()) 
 z = aa.y 
-print(z(())) 
-print(a().y((a,))) 
-print(A.y(aa, (a,z))) 
-print(aa.y((z,1,'z')))
+print(z(()))  #Devuelve 0 ya que no hemos introducido valores dentro de len ---> len()
+print(a().y((a,)))   #Devuelve len(a), es decir, 1
+print(A.y(aa, (a,z)))   #Devuelve len(a,z), que es igual a 2 ya que (a,z) tiene 2 elementos
+print(aa.y((z,1,'z')))  #Devuelve len(z,1,'z'), que es 3 ya que tiene 3 elementos
