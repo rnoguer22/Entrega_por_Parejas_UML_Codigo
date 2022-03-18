502ee5ed41e3f84dc7cf7f8f49ef07011dd9b080
rnoguer22 <rnogugon@myuax.com>
2022-03-18 11:58:03 +0100

    Completamos la funcion llamada

--- Ejercicios/Logger.py
+++ Ejercicios/Logger.py
@@ -1,3 +1,12 @@
 class Test:
-    def llamada(self):
-        
+
+    def llamada(self, string):
+        file = open("cat log.txt", "w")
+        file.write(string)
+
+test = Test()
+for i in range(1,6): 
+   if i == 1: 
+       test.llamada("Primera llamada") 
+   else: 
+       test.llamada("{}ª llamada".format(i)) 
