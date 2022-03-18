fcbbce85ed1f47f336fe2c5eb3bd36287324353c
rnoguer22 <rnogugon@myuax.com>
2022-03-18 12:10:26 +0100

    Incluimos saltos de linea

--- Ejercicios/Logger.py
+++ Ejercicios/Logger.py
@@ -3,11 +3,11 @@ class Test:
         file.write("{}".format(string))
 
 file = open("cat log.txt", "w")
-file.write("--Start log--")
+file.write("--Start log--\n")
 
 test = Test()
 for i in range(1,6): 
    if i == 1: 
-       test.llamada("Primera llamada") 
-   else: 
-       test.llamada("{}ª llamada".format(i)) 
+       test.llamada("Primera llamada\n") 
+   else:
+       test.llamada("{}ª llamada\n".format(i)) 
