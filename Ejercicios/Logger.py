25d1acdce6bec4edce5de99f0ab0f8e19a81b6df
rnoguer22 <rnogugon@myuax.com>
2022-03-19 11:37:07 +0100

    Creamos el archivo main.py A su vez importamos el ejercio "Logger.py"

--- Ejercicios/Logger.py
+++ Ejercicios/Logger.py
@@ -1,22 +1,24 @@
 class Test:
     #metodo llamada
-    def llamada(self, string):
+    def llamada(self, string, file):
         #Escribe en file el argumento del metodo
         file.write("{}".format(string))
 
-#Abrimos el archivo y escribimos el mensaje inicial
-file = open("cat log.txt", "w")
-file.write("--Start log--\n")
+#Funcion que llamaremos desde el archivo mian.py
+def main():
+    #Abrimos el archivo y escribimos el mensaje inicial
+    file = open("cat log.txt", "w")
+    file.write("--Start log--\n")
 
-#Declaramos la instancia de clase
-test = Test()
-#Bucle para escribir en el archivo .txt
-for i in range(1,6): 
-   if i == 1: 
-       test.llamada("Primera llamada\n") 
-   else:
-        test.llamada("{}ª llamada\n".format(i)) 
-        if i == 5:
-            #Escribimos el mensaje final y cerramos el archivo
-            file.write("--End log: {} log(s)--".format(i))
-            file.close()
+    #Declaramos la instancia de clase
+    test = Test()
+    #Bucle para escribir en el archivo .txt
+    for i in range(1,6): 
+        if i == 1: 
+            test.llamada("Primera llamada\n", file) 
+        else:
+                test.llamada("{}ª llamada\n".format(i), file) 
+                if i == 5:
+                    #Escribimos el mensaje final y cerramos el archivo
+                    file.write("--End log: {} log(s)--".format(i))
+                    file.close()
---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+
+    from Ejercicios.Logger import main
+    main()
