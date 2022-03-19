bff46c983f213e3d18b60facee7fad0625a0d097
rnoguer22 <rnogugon@myuax.com>
2022-03-19 11:43:05 +0100

    Dcoramos el main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 if __name__ == "__main__":
 
     from Ejercicios.Logger import main
+    print ("Ejercicio Logger:")
     main()
+    print ("\n")
+
+    from Ejercicios.Puzzle import main
+    print ("Ejercicio Puzzle:")
+    main()
+    print ("\n")
