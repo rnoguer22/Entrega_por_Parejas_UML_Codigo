e9b5e2d66fa13c36aae195f71aae291bd3b71e4d
Pelayo <phuermij@myuax.com>
2022-03-22 14:16:53 +0100

    test

--- Ejercicios/palindromo2.py
+++ Ejercicios/palindromo2.py
@@ -1,2 +1,32 @@
+#Creamos la clase
+from readline import get_history_length
+
+
 class Palindromo2():
+
+    #Definimos el constructor
+    def __init__(self, palabra):
+        self.palabra = palabra
+
+    #Definimos el método test
+    def test(self):
+
+        while len(self.palabra) >= 1:
+            
+            if self.palabra[0] == self.palabra[-1]:
+                self.palabra = self.palabra[1:-1]
+                return self.test()
+
+            else:
+                return False
+
+        if len(self.palabra) < 1:
+            return True
+
+            
     
+
+p = Palindromo2("radar")
+print(p.test())
+
+
